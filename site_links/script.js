80f4e6a423f8f407ba14724d12166f89b168f25f
tiagosillos-art <tiagosillos.art@gmail.com>
2026-04-26 23:09:17 -0300

    Update script.js

--- site_links/script.js
+++ site_links/script.js
@@ -36,17 +36,17 @@ const ARTES_SIZE_OVERRIDES = {
   'tela_06.jpg': { scale: 1.0 },
   'tela_09.jpg': { scale: 1.0 },
   'tela_10.jpg': { scale: 1.0 },
-  'tela_11.jpg': { scale: 1.0 },
+  'tela_11.jpg': { scale: 1.5 },
   'tela_12.jpg': { scale: 1.0 },
   'tela_15.jpg': { scale: 1.0 },
   'tela_16.jpg': { scale: 1.0 },
   'tela_18.jpg': { scale: 1.0 },
   'tela_19.jpg': { scale: 2.0 },
-  'tela_26.jpg': { scale: 1.0 },
+  'tela_26.jpg': { scale: 1.5 },
   'tela_28.jpg': { scale: 1.0 },
-  'tela_30.jpg': { scale: 1.0 },
+  'tela_30.jpg': { scale: 1.5 },
   'tela_31.jpg': { scale: 1.0 },
-  'tela_32.jpg': { scale: 1.0 }
+  'tela_32.jpg': { scale: 1.5 }
 };
 
 // ------------------------------------------------------------
